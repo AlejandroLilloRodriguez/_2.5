7b57a86aad52be7d53ec94843209bd175cc8cf0c
AlejandroLilloRodriguez <alejandro.lillorodriguez@usp.ceu.es>
2023-02-16 13:46:38 +0100

    Ejercicio5 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,12 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+import dominio.Ejercicio5;
+
+public class Principal {
+    public static void main (String [] args){
+        int[] lista={1,3,4};
+        int i;
+        System.out.print("La media de los siguientes valores es:"+ Ejercicio5.media(lista,i=lista.length-1)/lista.length);
+
+    }
 }
--- src/dominio/Ejercicio5.java
+++ src/dominio/Ejercicio5.java
@@ -1,2 +1,11 @@
-package dominio;public class Ejercicio5 {
+package dominio;
+
+public class Ejercicio5 {
+    public static double media (int []lista,int i){
+        if(i==0)
+            return lista[i];
+        else
+            return (lista[i]+media(lista,i-1));
+
+    }
 }
